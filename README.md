cf361b075258f39345dcc0ee7caa201a58a4b2ef
Ben McNeill <bmcneill@logosresourcesllc.com>
2022-08-25 14:28:47 -0600

    create sql questions

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # SQL Optimization Mini Project
+
+## references
+
+- [git repo](https://github.com/bfmcneill/sql-optimization-mini-project)
+- [docker compose example](https://nickjanetakis.com/blog/docker-tip-88-switching-mysql-to-mariadb-for-arm-64-m1-support)
+- [env files with docker compose](https://docs.docker.com/compose/environment-variables/)
---
+++ sql-questions/q1.sql
@@ -0,0 +1,18 @@
+-- list the name of the student with id equal to v1 (id)
+
+USE springboardopt;
+
+
+SET @v1 = 1612521;
+SET @v2 = 1145072;
+SET @v3 = 1828467;
+SET @v4 = 'MGT382';
+SET @v5 = 'Amber Hill';
+SET @v6 = 'MGT';
+SET @v7 = 'EE';			  
+SET @v8 = 'MAT';
+
+-- 1. List the name of the student with id equal to v1 (id).
+SELECT name FROM Student WHERE id = @v1;
+q1.sql
+Displaying q1.sql.
